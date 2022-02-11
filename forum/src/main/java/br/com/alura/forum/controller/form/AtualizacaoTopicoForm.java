8bf113c03f3adf527053cb26d5a4b22b9400d9b9
Felipe Lauton <felipelauton021@gmail.com>
2022-02-11 10:07:16 -0300

    Metodo Put criado

--- forum/src/main/java/br/com/alura/forum/controller/form/AtualizacaoTopicoForm.java
+++ forum/src/main/java/br/com/alura/forum/controller/form/AtualizacaoTopicoForm.java
@@ -2,19 +2,18 @@ package br.com.alura.forum.controller.form;
 
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
+
 import org.hibernate.validator.constraints.Length;
-import br.com.alura.forum.modelo.Curso;
+
 import br.com.alura.forum.modelo.Topico;
-import br.com.alura.forum.repository.CursoRepository;
+import br.com.alura.forum.repository.TopicoRepository;
 
-public class TopicoForm {
-
+public class AtualizacaoTopicoForm {
+	
 	@NotNull @NotEmpty @Length(min = 5)
 	private String titulo;
 	@NotNull @NotEmpty @Length(min = 10)
 	private String mensagem;
-	@NotNull @NotEmpty
-	private String nomeCurso;
 	
 	public String getTitulo() {
 		return titulo;
@@ -28,14 +27,13 @@ public class TopicoForm {
 	public void setMensagem(String mensagem) {
 		this.mensagem = mensagem;
 	}
-	public String getNomeCurso() {
-		return nomeCurso;
-	}
-	public void setNomeCurso(String nomeCurso) {
-		this.nomeCurso = nomeCurso;
-	}
-	public Topico converter(CursoRepository repository) {
-		Curso curso = repository.findByNome(nomeCurso);
-		return new Topico(titulo, mensagem, curso);
+	
+	public Topico atualizar(Long id, TopicoRepository topicoRepository) {
+		Topico topico = topicoRepository.getById(id);
+		
+		topico.setTitulo(this.titulo);
+		topico.setMensagem(this.mensagem);
+		
+		return topico;
 	}
 }
